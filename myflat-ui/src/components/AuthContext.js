ace9eecd5a90978b98b976e66bb19f967d6e8ea6
MarkusMayer1 <markus.mayer1@stud.fh-campuswien.ac.at>
2024-06-15 19:42:58 +0200

    Added LoginPage.test.js

--- myflat-ui/src/components/AuthContext.js
+++ myflat-ui/src/components/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 export function useAuth() {
   return useContext(AuthContext);
